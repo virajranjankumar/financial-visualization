b56224e255c588d57b02f6b260ca10ebb75cd04f
Viraj Ranjankumar <viraj.ranjankumar@gmail.com>
2019-10-16 16:17:15 +0400

    Add Google analytics and removed login code

--- webpack.config.js
+++ webpack.config.js
@@ -21,27 +21,14 @@ function getStyleUse(bundleFilename) {
 
 module.exports = [
   {
-    entry: './login.scss',
-    output: {
-      // This is necessary for webpack to compile, but we never reference this js file.
-      filename: 'style-bundle-login.js',
-    },
-    module: {
-      rules: [{
-        test: /login.scss$/,
-        use: getStyleUse('bundle-login.css')
-      }]
-    },
-  },
-  {
-    entry: './home.scss',
+    entry: './index.scss',
     output: {
       // This is necessary for webpack to compile, but we never reference this js file.
       filename: 'style-bundle-home.js',
     },
     module: {
       rules: [{
-        test: /home.scss$/,
+        test: /index.scss$/,
         use: getStyleUse('bundle-home.css')
       }]
     },
@@ -73,26 +60,13 @@ module.exports = [
     },
   },
   {
-    entry: "./login.js",
-    output: {
-      filename: "bundle-login.js"
-    },
-    module: {
-      loaders: [{
-        test: /login.js$/,
-        loader: 'babel-loader',
-        query: {presets: ['env']}
-      }]
-    },
-  },
-  {
-    entry: "./home.js",
+    entry: "./index.js",
     output: {
       filename: "bundle-home.js"
     },
     module: {
       loaders: [{
-        test: /home.js$/,
+        test: /index.js$/,
         loader: 'babel-loader',
         query: {presets: ['env']}
       }]
