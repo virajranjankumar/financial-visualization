4a3723c28ed9e9b9c3de498844b95229af96f3a2
Viraj Ranjankumar <viraj.ranjankumar@gmail.com>
2019-10-17 12:49:41 +0400

    add a FAB for email contact

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import {MDCList} from '@material/list';
+import {MDCRipple} from '@material/ripple';
 
+const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
 new MDCList(document.querySelector('.mdc-list'));
